9b3e7945a8c3a944d0bd0feda113554c97bc43ac
ragegaming23 <damienagross@gmail.com>
2025-08-23 12:28:48 -0400

    messing around with the code to make a dash input work

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -18,8 +18,10 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move left", "move right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * SPEED 
+		if Input.is_action_just_pressed("Dash"): velocity.x = direction * SPEED * 2
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
