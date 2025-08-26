d03f01f1a1babda45bb05977402dc7be45b6e6d4
ragegaming23 <112526467+ragegaming23@users.noreply.github.com>
2025-08-26 16:57:48 -0400

    deleted respawn code

--- Test area/test_scene.gd
+++ Test area/test_scene.gd
@@ -2,9 +2,9 @@ extends Node2D
 
 
 @onready var marker_2d: Marker2D = $Marker2D
-func _ready() -> void:
-	for player in get_tree().Get_nodes_in_group("player"):
-		player.respawned.connect(_on_Player_respawned.bind(player))
+
+	
+	
 		
 func _on_player_respawned(player):
 	player.global_position = marker_2d.global_position
