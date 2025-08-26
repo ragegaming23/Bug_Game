481e26ca7911193c8841ac2d2ed7dfa3820f4e99
ragegaming23 <112526467+ragegaming23@users.noreply.github.com>
2025-08-26 12:52:05 -0400

    Added a main menu, character selection and map selection screen and made sure you can move between them to get to the test level

---
+++ UI/menu.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$VBoxContainer/Start.grab_focus()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://UI/character_selection.tscn")
+
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
+	get_tree() .quit()
