6fc67f2d079089c8e6ea8b37191dfa280c978184
ragegaming23 <112526467+ragegaming23@users.noreply.github.com>
2025-08-28 12:02:15 -0400

    working on knock back

--- Test area/knockback.gd
+++ Test area/knockback.gd
@@ -4,8 +4,8 @@ extends CollisionShape2D
 var knockback_dir = Vector2()
 var knockback_wait = 10
 
-for body in $".".get_overlapping_bodies():
+for body in $Knockback .get_overlapping_bodies():
 	if knockback_wait <= 0 and body.get("name") == "player"
-		print("knockback")
+		print("knockback") 
 		knockback_wait = 10
-knockback_wait
+knockback_wait -= 1
