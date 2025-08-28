b83dc73671c1969d43d448b5b26e01179a39ff83
ragegaming23 <112526467+ragegaming23@users.noreply.github.com>
2025-08-28 17:05:44 -0400

    made the enemy move towards the player

--- enemy.gd
+++ enemy.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
-@onready var terget=$"."
+@onready var terget = get_tree().get_first_node_in_group("Player")
 const Name = "enemy"
 var speed = 200
 
-func _physics_process(_delta):
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
 	var direction=(terget.position-position).normalized()
 	velocity=direction * speed
 	look_at(terget.position)
 	move_and_slide()
-	
