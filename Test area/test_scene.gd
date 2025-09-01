8eb117d11c16854069baaf9982a6ead8363fdab5
Franky Ortiz <156234855+VarsityS6@users.noreply.github.com>
2025-09-01 16:16:17 -0400

    READY?
    Added Announcer Lines
    Unable to make voice plays

--- Test area/test_scene.gd
+++ Test area/test_scene.gd
@@ -4,7 +4,18 @@ extends Node2D
 @onready var marker_2d: Marker2D = $Marker2D
 
 	
-	
-		
 func _on_player_respawned(player):
 	player.global_position = marker_2d.global_position
+	start_round()
+	
+func start_round():
+	# Play "Ready"
+	$Announcer.stream = preload("res://Voice Lines/Announcer/AnnouncerHall_Ready.wav")
+	$Announcer.play()
+	await $Announcer.finished # Wait until sound ends
+	
+	# Play "Fight"
+	$Announcer.stream=preload("res://Voice Lines/Announcer/AnnouncerHall_Fight.ogg")
+	$Announcer.play()
+	await $Announcer.finished
+	
