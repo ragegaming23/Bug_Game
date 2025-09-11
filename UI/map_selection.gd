07075fd50364a80a9a4653a0040dd9cbd5e907f2
ragegaming23 <damienagross@gmail.com>
2025-09-10 20:27:05 -0400

    META CONFRENCE CHANGES

--- UI/map_selection.gd
+++ UI/map_selection.gd
@@ -2,8 +2,9 @@ extends Control
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _ready():
+	$BoxContainer/Button.grab_focus()
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
