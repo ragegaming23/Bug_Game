912efe14b160c3b2998664cb637b1135bba4825e
Franky Ortiz <156234855+VarsityS6@users.noreply.github.com>
2025-09-25 12:56:52 -0400

    bruh idek
    Changed test scene

--- Test area/test_scene.gd
+++ Test area/test_scene.gd
@@ -4,12 +4,12 @@ extends Node2D
 @onready var announcer_ready = $AnnouncerReady
 @onready var announcer_fight = $AnnouncerFight
 @onready var bg_music = $PlaceholderOST
-
+var player_id 
 func _ready():
 	var spawn_name = Global.chosen_spawn
 	var spawn_points = $Spawnpoints
 	var spawn_point = spawn_points.get_node(spawn_name)
-	var player = $"Test player"
+	var player = "player_%s" %[player_id]
 	player.global_position = spawn_point.global_position
 	
 	announcer_ready.play()
