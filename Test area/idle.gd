5c35b4a5e117e46c0e5485bd44feb69500bd59f8
ragegaming23 <112526467+ragegaming23@users.noreply.github.com>
2025-10-16 16:43:10 -0400

    changing states

--- Test area/idle.gd
+++ Test area/idle.gd
@@ -3,15 +3,15 @@ class_name Idle
 
 @export var player: CharacterBody2D
 @export var SPEED:= 0
-const is_on_floor()
+
 
 func enter() -> void:
 	$Animantis.play("idle")
 
-func _process(delta: float) -> void:
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+#func _process(delta: float) -> void:
+	#if not is_on_floor():
+	#velocity += get_gravity() * delta
 
-func _physics_process(delta) -> void:
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+#func _physics_process(delta) -> void:
+	#if not is_on_floor():
+	#velocity += get_gravity() * delta
