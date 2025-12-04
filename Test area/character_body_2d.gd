4f6e96a072123ed1e1809dffc0758c5d057985c3
ragegaming23 <112526467+ragegaming23@users.noreply.github.com>
2025-12-04 11:58:39 -0500

    damage change and the rest of the game

--- Test area/character_body_2d.gd
+++ Test area/character_body_2d.gd
@@ -31,7 +31,15 @@ func _ready():
 	emit_signal("lives_changed", lives)
 
 
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+# ---------------- DAMAGE SYSTEM ----------------
+func Take_Damage(dmg: int):
+=======
+>>>>>>> Stashed changes
 func take_damage(dmg: int):
+>>>>>>> 5cd558f7a616be90e4f0c29bdd9b6b6ce844b99e
 	current_health = max(current_health - dmg, 0)
 
 	if has_node("HealthSFX"):
@@ -95,4 +103,4 @@ func _on_area_2d_body_entered(body):
 			body.apply_knockback(direction, 200.0, 1.0)
 
 		if body.has_method("take_damage"):
-			body.take_damage(1)
+			body.Take_Damage(1)
